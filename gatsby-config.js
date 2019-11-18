d0d81323342bb53f044fa00ce996cc56cb7036c1
VietED Deployment <vieted.deployment@gmail.com>
2019-11-19 00:54:26 +0500

    First demo Gatsby for Steve by Harry Ha

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `First demo Gatsby for Steve by Harry Ha`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
